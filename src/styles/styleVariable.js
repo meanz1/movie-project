140c03fee7b61ac5e91fad64ad939a1ab6a7051d
meanz1 <zndnfjqbd1206@gmail.com>
2023-11-21 23:39:18 +0900

    [style] 전역 스타일 및 팔레트 설정 #1

---
+++ src/styles/styleVariable.js
@@ -0,0 +1,11 @@
+const Palette = {
+  LightGray: "#DCDCDC",
+  MediumGray: "#BEBEBE",
+  DarkGray: "#545454",
+  Black: "#232323",
+  Yellow: "#F8FEB5",
+  SkyBlue: "#C5E1DB",
+  LightSkyBlue: "#D8EDE7",
+};
+
+export default Palette;
